5c0d2e7d218973c93a9f41d3252a3de7371fc6e9
Gyorgy Rethy <rethygy0410@gmail.com>
2020-07-17 11:57:15 +0100

    [feat] Add native OpenCL example that reads and runs SPIR-V

---
+++ examples/vector_add_il.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <CL/opencl.h>
+
+int main( int argc, char* argv[] )
+{
+	FILE *f = fopen(argv[1], "rb");
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	char *string = malloc(fsize + 1);
+	fread(string, 1, fsize, f);
+	fclose(f);
+
+	string[fsize] = 0;
+
+    // Length of vectors
+    unsigned int n = 100000;
+ 
+    // Host input vectors
+    double *h_a;
+    double *h_b;
+    // Host output vector
+    double *h_c;
+ 
+    // Device input buffers
+    cl_mem d_a;
+    cl_mem d_b;
+    // Device output buffer
+    cl_mem d_c;
+ 
+    cl_device_id device_id;           // device ID
+    cl_context context;               // context
+    cl_command_queue queue;           // command queue
+    cl_program program;               // program
+    cl_kernel kernel;                 // kernel
+ 
+    // Size, in bytes, of each vector
+    size_t bytes = n*sizeof(double);
+ 
+    // Allocate memory for each vector on host
+    h_a = (double*)malloc(bytes);
+    h_b = (double*)malloc(bytes);
+    h_c = (double*)malloc(bytes);
+ 
+    // Initialize vectors on host
+    int i;
+    for( i = 0; i < n; i++ )
+    {
+        h_a[i] = sinf(i)*sinf(i);
+        h_b[i] = cosf(i)*cosf(i);
+    }
+ 
+    size_t globalSize, localSize;
+    cl_int err;
+ 
+    // Number of work items in each local work group
+    localSize = 64;
+ 
+    // Number of total work items - localSize must be devisor
+    globalSize = ceil(n/(float)localSize)*localSize;
+ 
+    // Bind to platform
+    cl_uint num_platforms;
+    err = clGetPlatformIDs(1, NULL, &num_platforms);
+    cl_platform_id all_platforms[num_platforms];
+    err = clGetPlatformIDs(num_platforms, &all_platforms, NULL);
+ 
+    // Get ID for the device
+    err = clGetDeviceIDs(all_platforms[num_platforms - 1], CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
+
+    //Print name of the device
+    size_t name_size;
+    err = clGetDeviceInfo(device_id, CL_DEVICE_NAME, 0, NULL, &name_size);
+    char name[name_size];
+    err = clGetDeviceInfo(device_id, CL_DEVICE_NAME, name_size, name, &name_size);
+    printf("%s\n", name);
+ 
+    // Create a context 
+    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+ 
+    // Create a command queue
+    queue = clCreateCommandQueue(context, device_id, 0, &err);
+ 
+    // Create the compute program from the source buffer
+    program = clCreateProgramWithIL(context, string, fsize, &err);
+ 
+    // Build the program executable
+    clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+ 
+    // Create the compute kernel in the program we wish to run
+    kernel = clCreateKernel(program, "vecAdd", &err);
+ 
+    // Create the input and output arrays in device memory for our calculation
+    d_a = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
+    d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
+    d_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
+ 
+    // Write our data set into the input array in device memory
+    err = clEnqueueWriteBuffer(queue, d_a, CL_TRUE, 0,
+                                   bytes, h_a, 0, NULL, NULL);
+    err |= clEnqueueWriteBuffer(queue, d_b, CL_TRUE, 0,
+                                   bytes, h_b, 0, NULL, NULL);
+ 
+    // Set the arguments to our compute kernel
+    err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+    err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+    err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &n);
+ 
+    // Execute the kernel over the entire range of the data set 
+    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize,
+                                                              0, NULL, NULL);
+ 
+    // Wait for the command queue to get serviced before reading back results
+    clFinish(queue);
+ 
+    // Read the results from the device
+    clEnqueueReadBuffer(queue, d_c, CL_TRUE, 0,
+                                bytes, h_c, 0, NULL, NULL );
+ 
+    //Sum up vector c and print result divided by n, this should equal 1 within error
+    double sum = 0;
+    for(i=0; i<n; i++)
+        sum += h_c[i];
+    printf("final result: %f\n", sum/n);
+ 
+    // release OpenCL resources
+    clReleaseMemObject(d_a);
+    clReleaseMemObject(d_b);
+    clReleaseMemObject(d_c);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
+ 
+    //release host memory
+    free(h_a);
+    free(h_b);
+    free(h_c);
+ 
+    return 0;
+}
