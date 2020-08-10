d3e1c94b2fa68c62aa3e77a3944a57555787d408
Gyorgy Rethy <rethygy0410@gmail.com>
2020-08-10 10:58:39 +0100

    [fix] Fix enum operands with parameters not calculating parameters into wordcount

--- lib/src/main/java/uk/ac/manchester/spirvproto/lib/instructions/operands/SPIRVEnum.java
+++ lib/src/main/java/uk/ac/manchester/spirvproto/lib/instructions/operands/SPIRVEnum.java
@@ -1,12 +1,15 @@
 package uk.ac.manchester.spirvproto.lib.instructions.operands;
 
 import java.nio.ByteBuffer;
+import java.util.List;
 
 public abstract class SPIRVEnum implements SPIRVOperand{
     protected final int value;
+    protected final List<SPIRVOperand> parameters;
 
-    protected SPIRVEnum(int value) {
+    protected SPIRVEnum(int value, List<SPIRVOperand> parameters) {
         this.value = value;
+        this.parameters = parameters;
     }
 
     @Override
@@ -16,6 +19,6 @@ public abstract class SPIRVEnum implements SPIRVOperand{
 
     @Override
     public int getWordCount() {
-        return 1;
+        return 1 + parameters.stream().mapToInt(SPIRVOperand::getWordCount).sum();
     }
 }
