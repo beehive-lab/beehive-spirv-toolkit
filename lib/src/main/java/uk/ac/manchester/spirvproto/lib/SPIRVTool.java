3b4d59710a25e94994ff379415f0d94d8b189a29
Gyorgy Rethy <rethygy0410@gmail.com>
2020-08-13 17:53:25 +0100

    [feat] Add assembler to the runner, so that it can be used through the spirv-proto command

--- lib/src/main/java/uk/ac/manchester/spirvproto/lib/SPIRVTool.java
+++ lib/src/main/java/uk/ac/manchester/spirvproto/lib/SPIRVTool.java
@@ -1,5 +1,7 @@
 package uk.ac.manchester.spirvproto.lib;
 
+import uk.ac.manchester.spirvproto.lib.assembler.InvalidSPIRVModuleException;
+
 public interface SPIRVTool {
-    void run() throws Exception;
+    void run() throws Exception, InvalidSPIRVModuleException;
 }
--- lib/src/main/java/uk/ac/manchester/spirvproto/lib/assembler/InvalidSPIRVModuleException.java
+++ lib/src/main/java/uk/ac/manchester/spirvproto/lib/assembler/InvalidSPIRVModuleException.java
@@ -1,6 +1,6 @@
 package uk.ac.manchester.spirvproto.lib.assembler;
 
-public class InvalidSPIRVModuleException extends Throwable {
+public class InvalidSPIRVModuleException extends Exception {
     public InvalidSPIRVModuleException(String message) {
         super(message);
     }
