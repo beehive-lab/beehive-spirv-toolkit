780f43105a8d519bb3257510f2e618c2b2fca47a
Gyorgy Rethy <rethygy0410@gmail.com>
2020-08-21 11:19:55 +0100

    [test] Add tester script that disassembles, assembles and then validates all of the rodinia benchmark kernels.

---
+++ examples/rodinia-test/run-asm-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILES=${1:-$(find ../rodinia -type f -name "*.spv")}
+
+# Check all files
+for FILE in $FILES; do
+	# Assemble
+	java -jar ../../dist/spirv-proto.jar -d --no-grouping $FILE -o proto/out.spvasm
+	java -jar ../../dist/spirv-proto.jar -d --tool asm -o proto/out.spv proto/out.spvasm
+
+	# Validate
+	if spirv-val proto/out.spv; then
+		echo "$FILE success"
+	else 
+		echo "$FILE fail"
+	fi
+done
