7156c10f0bfae3669d5323d1052569e10b72a540
Juan Fumero <jjfumero@gmail.com>
2021-03-17 16:16:48 +0100

    Docs and tuning for TornadoVM

--- lib/src/main/java/uk/ac/manchester/spirvproto/lib/assembler/SPIRVToken.java
+++ lib/src/main/java/uk/ac/manchester/spirvproto/lib/assembler/SPIRVToken.java
@@ -7,11 +7,16 @@ class SPIRVToken {
     public SPIRVToken(String token, SPIRVInstRecognizer recognizer) {
         value = token.replace("\\s", "");
 
-        if (value.equals("=")) type = SPIRVTokenType.ASSIGN;
-        else if (value.startsWith(";")) type = SPIRVTokenType.COMMENT;
-        else if (value.startsWith("%")) type = SPIRVTokenType.ID;
-        else if (recognizer.isInstruction(token)) type = SPIRVTokenType.INSTRUCTION;
-        else type = SPIRVTokenType.IMMEDIATE;
+        if (value.equals("="))
+            type = SPIRVTokenType.ASSIGN;
+        else if (value.startsWith(";"))
+            type = SPIRVTokenType.COMMENT;
+        else if (value.startsWith("%"))
+            type = SPIRVTokenType.ID;
+        else if (recognizer.isInstruction(token))
+            type = SPIRVTokenType.INSTRUCTION;
+        else
+            type = SPIRVTokenType.IMMEDIATE;
     }
 
     @Override
