429e30886c20aad4d57704518bcc9c161b3db818
Juan Fumero <jjfumero@gmail.com>
2021-03-24 07:39:51 +0100

    Checker Get platform for OpenCL 3.0

--- examples/vector_add/vector_add.cpp
+++ examples/vector_add/vector_add.cpp
@@ -24,9 +24,9 @@ int get_platform_with_2_1(cl_platform_id *ids, int num_platforms) {
 		if (clGetPlatformInfo(ids[i], CL_PLATFORM_VERSION, version_length, version, &version_length) != CL_SUCCESS) {
 				puts("ERROR: clGetPlatformInfo failed"); return -1;
 		}
-		if (strlen(version) >= 10 && (strncmp(version, "OpenCL 2.1", 10) == 0)) {
-			//return i;
-			deviceNumber = i;
+		if (strlen(version) >= 10 && (strncmp(version, "OpenCL 3.0", 10) == 0)) {
+			return i;
+			//deviceNumber = i;
 		}
 	}
 	return deviceNumber;
