32e5f273a63b66d997f5cf3375d80eb83ed70e7d
Juan Fumero <juan.fumero@manchester.ac.uk>
2021-04-30 15:21:44 +0200

    Vector addition added with the ASM API

--- lib/src/main/java/uk/ac/manchester/spirvproto/lib/tests/TestVector.java
+++ lib/src/main/java/uk/ac/manchester/spirvproto/lib/tests/TestVector.java
@@ -13,6 +13,7 @@ import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpExtInstImport;
 import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpFunction;
 import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpFunctionEnd;
 import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpFunctionParameter;
+import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpIAdd;
 import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpInBoundsPtrAccessChain;
 import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpLabel;
 import uk.ac.manchester.spirvproto.lib.instructions.SPIRVOpLoad;
@@ -303,7 +304,7 @@ public class TestVector {
                 new SPIRVHeader(
                         1,
                         2,
-                        29,    // First ID avaialble for SPIR-V
+                        29,    // First ID available for SPIR-V
                         0,
                         0));
         SPIRVInstScope functionScope;
@@ -536,10 +537,314 @@ public class TestVector {
         writeModuleToFile(module,"/tmp/testSPIRV2.spv");
     }
 
+    /**
+     * Test for Vector Addition:
+     *
+     * <code>
+     *      __kernel void vectorAdd(__global int* a, __global int *b, __global int *c) {
+     * 	        int idx = get_global_id(0);
+     * 	        a[idx] = b[idx] + c[idx];
+     *      }
+     * </code>
+     *
+     */
+    public static void testVectorAdd() {
+
+        // SPIRV Header
+        SPIRVModule module = new SPIRVModule(
+                new SPIRVHeader(
+                        1,
+                        2,
+                        29,    // First ID available for SPIR-V
+                        0,
+                        0));
+        SPIRVInstScope functionScope;
+        SPIRVInstScope blockScope;
+
+        module.add(new SPIRVOpCapability(SPIRVCapability.Addresses()));     // Uses physical addressing, non-logical addressing modes.
+        module.add(new SPIRVOpCapability(SPIRVCapability.Linkage()));       // Uses partially linked modules and libraries. (e.g., OpenCL)
+        module.add(new SPIRVOpCapability(SPIRVCapability.Kernel()));        // Uses the Kernel Execution Model.
+        module.add(new SPIRVOpCapability(SPIRVCapability.Int64()));         // Uses OpTypeInt to declare 64-bit integer types
+
+        // Add import OpenCL STD
+        SPIRVId idImport = module.getNextId();
+        module.add(new SPIRVOpExtInstImport(idImport, new SPIRVLiteralString("OpenCL.std")));
+
+        // Set the memory model to Physical64 with OpenCL
+        module.add(new SPIRVOpMemoryModel(SPIRVAddressingModel.Physical64(), SPIRVMemoryModel.OpenCL()));
+
+        SPIRVId idSPIRVBuiltin = module.getNextId();
+
+        // Add module entry function
+        SPIRVId mainFunctionID = module.getNextId();
+        module.add(new SPIRVOpEntryPoint(
+                SPIRVExecutionModel.Kernel(),
+                mainFunctionID,
+                new SPIRVLiteralString("testVectorAdd"),
+                new SPIRVMultipleOperands<>(idSPIRVBuiltin)
+        ));
+
+
+        // OpSource
+        module.add(new SPIRVOpSource(
+                SPIRVSourceLanguage.OpenCL_C(),
+                new SPIRVLiteralInteger(100000),
+                new SPIRVOptionalOperand<>(),
+                new SPIRVOptionalOperand<>()));
+
+        // Decoration
+        // Auxiliary information such as built-in variable, stream numbers, invariance, interpolation type,
+        // relaxed precision, etc., added to <id>s or structure-type members through Decorations.
+
+        // Add Decorators for the GetGlobalID intrinsics
+        module.add(new SPIRVOpDecorate(idSPIRVBuiltin, SPIRVDecoration.BuiltIn(SPIRVBuiltIn.GlobalInvocationId())));
+        module.add(new SPIRVOpDecorate(idSPIRVBuiltin, SPIRVDecoration.Constant()));
+        module.add(new SPIRVOpDecorate(idSPIRVBuiltin, SPIRVDecoration.LinkageAttributes(new SPIRVLiteralString("spirv_BuiltInGlobalInvocationId"), SPIRVLinkageType.Import())));
+
+        // Decorates for alignment of data
+        SPIRVId idx = module.getNextId();
+        SPIRVId a_addr = module.getNextId();
+        SPIRVId b_addr = module.getNextId();
+        SPIRVId c_addr = module.getNextId();
+        module.add(new SPIRVOpDecorate(idx, SPIRVDecoration.Alignment(new SPIRVLiteralInteger(4))));
+        module.add(new SPIRVOpDecorate(a_addr, SPIRVDecoration.Alignment(new SPIRVLiteralInteger(8))));
+        module.add(new SPIRVOpDecorate(b_addr, SPIRVDecoration.Alignment(new SPIRVLiteralInteger(8))));
+        module.add(new SPIRVOpDecorate(c_addr, SPIRVDecoration.Alignment(new SPIRVLiteralInteger(8))));
+
+        // Type declaration
+        SPIRVId ulong = module.getNextId();
+        SPIRVId uint = module.getNextId();
+        SPIRVId v3ulong = module.getNextId();
+        module.add(new SPIRVOpTypeInt(ulong, new SPIRVLiteralInteger(64), new SPIRVLiteralInteger(0)));
+        module.add(new SPIRVOpTypeInt(uint, new SPIRVLiteralInteger(32), new SPIRVLiteralInteger(0)));
+        // Vector of 3 longs
+        module.add(new SPIRVOpTypeVector(v3ulong, ulong, new SPIRVLiteralInteger(3)));
+
+        // Type pointers
+        // OpPointerType: Declare a new pointer type.
+        // Storage Class is the Storage Class of the memory holding the object pointed to. If there was a
+        // forward reference to this type from an OpTypeForwardPointer, the Storage Class of that instruction
+        // must equal the Storage Class of this instruction.
+        // pointer to the vector ulong
+        SPIRVId ptrInputv3ulong = module.getNextId();
+        // INPUT Class: Input from pipeline. Visible across all functions in the current invocation. Variables
+        // declared with this storage class are read-only, and must not have initializers.
+        module.add(new SPIRVOpTypePointer(ptrInputv3ulong, SPIRVStorageClass.Input(), v3ulong));
+
+        // Type void
+        SPIRVId voidType = module.getNextId();
+        module.add(new SPIRVOpTypeVoid(voidType));
+
+        // pointer to a crossGroup of uint
+        SPIRVId ptrCrossGroupUint = module.getNextId();
+        // CrossWorkGroup: Shared across all invocations within a work group. Visible across all functions.
+        // The OpenGL "shared" storage qualifier. OpenCL local memory.
+        module.add(new SPIRVOpTypePointer(ptrCrossGroupUint, SPIRVStorageClass.CrossWorkgroup(), uint));
+
+        // Pointer to a function
+        // %9 = OpTypeFunction %void %_ptr_CrossWorkgroup_uint %_ptr_CrossWorkgroup_uint
+        SPIRVId mainFunctionPre = module.getNextId();
+
+        // Declaration of the function with all parameter types
+        module.add(new SPIRVOpTypeFunction(mainFunctionPre, voidType, new SPIRVMultipleOperands<>(ptrCrossGroupUint, ptrCrossGroupUint, ptrCrossGroupUint)));
+
+        // ptrFunctionToPtrCrossWorkGroup ---> I dont get why we need this
+        SPIRVId ptrFunctionToPtrCrossWorkGroup = module.getNextId();
+        // FUNCTION: Visible only within the declaring function of the current invocation. Regular function memory.
+        module.add(new SPIRVOpTypePointer(ptrFunctionToPtrCrossWorkGroup, SPIRVStorageClass.Function(), ptrCrossGroupUint));
+
+        // Pointer to a function of type uint
+        SPIRVId ptrFunctionUint = module.getNextId();
+        module.add(new SPIRVOpTypePointer(ptrFunctionUint, SPIRVStorageClass.Function(), uint));
+
+        // Variable declaration
+        module.add(new SPIRVOpVariable(ptrInputv3ulong, idSPIRVBuiltin, SPIRVStorageClass.Input(), new SPIRVOptionalOperand<>()));
+
+        // Function Declaration
+        functionScope = module.add(new SPIRVOpFunction(voidType, mainFunctionID, SPIRVFunctionControl.DontInline(), mainFunctionPre));
+
+        SPIRVId a = module.getNextId();
+        SPIRVId b = module.getNextId();
+        SPIRVId c = module.getNextId();
+
+        // Parameter Declaration
+        functionScope.add(new SPIRVOpFunctionParameter(ptrCrossGroupUint, a));   // Buffer A
+        functionScope.add(new SPIRVOpFunctionParameter(ptrCrossGroupUint, b));   // Buffer B
+        functionScope.add(new SPIRVOpFunctionParameter(ptrCrossGroupUint, c));   // Buffer B
+
+        SPIRVOpLabel entry = new SPIRVOpLabel(module.getNextId());
+        blockScope = functionScope.add(entry);
+
+        blockScope.add(new SPIRVOpVariable(ptrFunctionToPtrCrossWorkGroup, a_addr, SPIRVStorageClass.Function(), new SPIRVOptionalOperand<>()));
+        blockScope.add(new SPIRVOpVariable(ptrFunctionToPtrCrossWorkGroup, b_addr, SPIRVStorageClass.Function(), new SPIRVOptionalOperand<>()));
+        blockScope.add(new SPIRVOpVariable(ptrFunctionToPtrCrossWorkGroup, c_addr, SPIRVStorageClass.Function(), new SPIRVOptionalOperand<>()));
+        blockScope.add(new SPIRVOpVariable(ptrFunctionUint, idx, SPIRVStorageClass.Function(), new SPIRVOptionalOperand<>()));
+
+        blockScope.add(new SPIRVOpStore(
+                a_addr,
+                a,
+                new SPIRVOptionalOperand<>(
+                        SPIRVMemoryAccess.Aligned(
+                                new SPIRVLiteralInteger(8)))
+        ));
+
+        blockScope.add(new SPIRVOpStore(
+                b_addr,
+                b,
+                new SPIRVOptionalOperand<>(
+                        SPIRVMemoryAccess.Aligned(
+                                new SPIRVLiteralInteger(8)))
+        ));
+
+        blockScope.add(new SPIRVOpStore(
+                c_addr,
+                c,
+                new SPIRVOptionalOperand<>(
+                        SPIRVMemoryAccess.Aligned(
+                                new SPIRVLiteralInteger(8)))
+        ));
+
+        // Call Thread-ID getGlobalId(0)
+        SPIRVId id19 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                v3ulong,
+                id19,
+                idSPIRVBuiltin,
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(32)))
+        ));
+
+        // Intrinsic call
+        SPIRVId call = module.getNextId();
+        blockScope.add(new SPIRVOpCompositeExtract(ulong, call, id19, new SPIRVMultipleOperands<>(new SPIRVLiteralInteger(0))));
+
+        SPIRVId conv = module.getNextId();
+        // SPIRVOpUConvert: Convert unsigned width. This is either a truncate or a zero extend.
+        blockScope.add(new SPIRVOpUConvert(uint, conv, call));
+
+        // Store the globalIdx into idx variable
+        blockScope.add(new SPIRVOpStore(idx, conv,new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(4)))));
+
+        // Load A[i] and b[i] using the thread index stored in idx
+        SPIRVId id22 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                ptrCrossGroupUint,
+                id22,
+                b_addr,    // Load B[i]
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(8)))
+        ));
+
+        SPIRVId id23 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                uint,
+                id23,
+                idx,    // Load B[i]
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(4)))
+        ));
+        SPIRVId idxprom = module.getNextId();
+        // SPIRVOpSConvert: Convert signed width. This is either a truncate or a sign extend.
+        blockScope.add(new SPIRVOpSConvert(ulong, idxprom, id23));
+
+        SPIRVId ptridx = module.getNextId();
+        blockScope.add(new SPIRVOpInBoundsPtrAccessChain(
+                ptrCrossGroupUint,
+                ptridx,
+                id22,
+                idxprom,
+                new SPIRVMultipleOperands()
+        ));
+
+        SPIRVId id28 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                uint,
+                id28,
+                ptridx,
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(4)))
+        ));
+
+        SPIRVId id29 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                ptrCrossGroupUint,
+                id29,
+                c_addr,    // Load C[i]
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(8)))
+        ));
+
+        SPIRVId id30 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                uint,
+                id30,
+                idx,
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(4)))
+        ));
+
+        SPIRVId idxprom1 = module.getNextId();
+        blockScope.add(new SPIRVOpSConvert(ulong, idxprom1, id30));
+
+        SPIRVId ptridx2 = module.getNextId();
+        blockScope.add(new SPIRVOpInBoundsPtrAccessChain(
+                ptrCrossGroupUint,
+                ptridx2,
+                id29,
+                idxprom1,
+                new SPIRVMultipleOperands()
+        ));
+
+        ///// Perform ADD
+        SPIRVId id33 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                uint,
+                id33,
+                ptridx2,
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(8)))
+        ));
+
+        SPIRVId add = module.getNextId();
+        blockScope.add(new SPIRVOpIAdd(uint, add, id28, id33));
+
+        SPIRVId id35 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                ptrCrossGroupUint,
+                id35,
+                a_addr,    // Load A[i]
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(8)))
+        ));
+
+        SPIRVId id36 = module.getNextId();
+        blockScope.add(new SPIRVOpLoad(
+                uint,
+                id36,
+                idx,
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(8)))
+        ));
+
+        SPIRVId idxprom3 = module.getNextId();
+        blockScope.add(new SPIRVOpSConvert(ulong, idxprom3, id36));
+
+        SPIRVId ptridx4 = module.getNextId();
+        blockScope.add(new SPIRVOpInBoundsPtrAccessChain(
+                ptrCrossGroupUint,
+                ptridx4,
+                id35,
+                idxprom3,
+                new SPIRVMultipleOperands()
+        ));
+
+        blockScope.add(new SPIRVOpStore(
+                ptridx4,
+                add,
+                new SPIRVOptionalOperand<>(SPIRVMemoryAccess.Aligned(new SPIRVLiteralInteger(4)))
+        ));
+
+        blockScope.add(new SPIRVOpReturn());
+        functionScope.add(new SPIRVOpFunctionEnd());
+
+        writeModuleToFile(module,"/tmp/testSPIRV3.spv");
+    }
 
     public static void main(String[] args) throws InvalidSPIRVModuleException {
         //testVectorInit();
-        testVectorCopy();
+        //testVectorCopy();
+        testVectorAdd();
     }
 
 }
